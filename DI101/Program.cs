3b6b4f46cf50236622f840453aabc325ccaa1c2e
abhinav <abhinav.kant@gmail.com>
2017-06-24 11:06:33 -0700

    simeple DI

--- DI101/Program.cs
+++ DI101/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DI101
 {
+    public interface IService
+    {
+        void Serve();
+    }
+
+    public class Service : IService
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Service Invoked");
+        }
+    }
+
+    public class Client
+    {
+        private readonly IService _service;
+
+        public Client(IService service)
+        {
+            _service = service;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Service Started");
+            this._service.Serve();
+            //To Do: Some Stuff
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var c = new Client(new Service());
+            c.Start();
+
+            Console.ReadKey();
         }
     }
 }
